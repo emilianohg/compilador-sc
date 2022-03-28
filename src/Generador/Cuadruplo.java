ba722af6bcf801665ff4f61bdbd57b716e39f225
Emiliano Hernández Guerrero <emiliano4144@hotmail.com>
2022-03-28 05:41:57 -0700

    Ventaana

---
+++ src/Generador/Cuadruplo.java
@@ -0,0 +1,40 @@
+
+package Generador;
+
+public class Cuadruplo {
+    private final String operator;
+    private final String arg0;
+    private final String arg1;
+    private final String res;
+    
+    public Cuadruplo(String operator, String arg0, String arg1, String res) {
+        this.operator = operator;
+        this.arg0 = arg0;
+        this.arg1 = arg1;
+        this.res = res;
+        System.out.println(this.toString());
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public String getArg0() {
+        return arg0;
+    }
+
+    public String getArg1() {
+        return arg1;
+    }
+
+    public String getRes() {
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return operator + "\t" + arg0 + "\t" + arg1 + "\t" + res;
+    }
+    
+    
+}
---
+++ src/Interfaz/Identifier.java
@@ -0,0 +1,43 @@
+package Interfaz;
+
+public class Identifier {
+    private final String type;
+    private final String name;
+    private final String value;
+    private final int size;
+    private final int pos;
+
+    public Identifier(String type, String name, String value, int size, int pos) {
+        this.type = type;
+        this.name = name;
+        this.value = value;
+        this.size = size;
+        this.pos = pos;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    @Override
+    public String toString() {
+        return "Identifier{" + "type=" + type + ", name=" + name + ", value=" + value + ", size=" + size + ", pos=" + pos + '}';
+    }
+    
+}
